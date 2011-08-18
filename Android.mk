f74d8e2c179065bd8712b113b65957c617ad6c54
PaulK <contact@paulk.fr>
2011-08-18 22:39:58 +0200

    devices: add support for Nexus S
    As for now, it features:
    * booting the modem
    * the ability to receive and send messages from/to the modem
    
    The messages received from the modem are in a known format and can be
    understood easilly, and it's also possible to send messages to the modem
    in that very same known format.

--- Android.mk
+++ Android.mk
@@ -13,6 +13,12 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_SRC_FILES := $(libsamsung-ipc_files)
 
+ifeq ($(TARGET_DEVICE),crespo)
+       libsamsung-ipc_files += devices/$(TARGET_DEVICE)/nv_data.c
+       LOCAL_CFLAGS += -Iexternal/openssl/include
+       LOCAL_LDFLAGS += -lcrypto
+endif
+
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -25,6 +31,12 @@ LOCAL_SHARED_LIBRARIES := libutils
 LOCAL_CFLAGS := -DLOG_STDOUT
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_SRC_FILES := $(libsamsung-ipc_files) test.c
+
+ifeq ($(TARGET_DEVICE),crespo)
+       libsamsung-ipc_files += devices/$(TARGET_DEVICE)/nv_data.c
+       LOCAL_CFLAGS += -Iexternal/openssl/include
+       LOCAL_LDFLAGS += -lcrypto
+endif
 
 include $(BUILD_EXECUTABLE)
 
---
+++ devices/crespo/nv_data.h
@@ -0,0 +1,37 @@
+/**
+ * This file is part of libsamsung-ipc.
+ *
+ * Copyright (C) 2011 Paul Kocialkowski <contact@paulk.fr>
+ *
+ * libsamsung-ipc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libsamsung-ipc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libsamsung-ipc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __CRESPO_NV_DATA_H__
+#define __CRESPO_NV_DATA__
+
+#define	NV_DATA_MD5_SECRET	"Samsung_Android_RIL"
+#define NV_DATA_SIZE		0x200000
+
+#define MD5_STRING_SIZE		MD5_DIGEST_LENGTH * 2 + 1
+
+void md5hash2string(char *out, uint8_t *in);
+void nv_data_generate(void);
+void nv_data_md5_compute(void *data_p, int size, void *hash);
+void nv_data_backup_create(void);
+void nv_data_backup_restore(void);
+void nv_data_check(void);
+void nv_data_md5_check(void);
+
+#endif
